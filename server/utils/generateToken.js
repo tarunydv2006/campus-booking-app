0e710b1a065e13b52964954ac020964a4d7f0e2e
tarunydv2006 <tarunyadav.27484@gmail.com>
2026-06-30 20:22:24 -0700

    fixed the API

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (user) => {
+   console.log("JWT_SECRET:", process.env.JWT_SECRET);
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET is missing from backend .env');
   }
